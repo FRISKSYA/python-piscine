a52c1d8430e49195993579c912081ea5dffbb120
FRISKSYA <frics5090fksk@gmail.com>
2026-02-25 13:17:17 +0900

    fix flake8

--- 0/ex01/format_ft_time.py
+++ 0/ex01/format_ft_time.py
@@ -2,7 +2,8 @@ import time
 from datetime import datetime
 
 seconds = time.time()
-print(f"Seconds since January 1, 1970: {seconds:,.4f} or {seconds:.2e} in scientific notation")
+print(f"Seconds since January 1, 1970: "
+      f"{seconds:,.4f} or {seconds:.2e} in scientific notation")
 
 now = datetime.now()
 print(now.strftime("%b %d %Y"))
